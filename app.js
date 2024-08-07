2cf7405a1d87ac8036833a5ff44592d9f88cc5b6
MohiVs <mohisls2012@gmail.com>
2024-08-07 13:35:50 +0330

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+
+app.use(express.json());
+// app.get('/', (req, res) => {
+//   res.status(200).json({ messaage: 'Hello wo' });
+// });
+
+// app.post('/', (req, res) => {
+//   res.send('you send somthing');
+// });
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8')
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res
+    .status(200)
+    .json({ status: 'success', result: tours.length, data: { tours } });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours.at(tours.length - 1).id + 1;
+  const newTour = { id: newId, ...req.body };
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        result: tours.length,
+        data: { tour: newTour },
+      });
+    }
+  );
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log('app is running');
+});
