59d75a242578a5117f955e4cb1a90249586155cc
mohsen <mohisls2012@gmail.com>
2024-08-10 21:15:31 +0330

    sd

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
-console.log(process.env);
+// console.log(process.env);
 
 const port = 3000;
 // const port = process.env.PORT || 3000;
@@ -10,5 +10,3 @@ const port = 3000;
 app.listen(port, () => {
   console.log('app is running');
 });
-
-// npm i eslint prettier eslint-config-prettier eslint-plugin-prettier eslint-config-airbnb eslint-plugin-node eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react --save-dev
