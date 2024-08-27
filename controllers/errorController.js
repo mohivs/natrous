82fe9a5209c2dc79e99d7fb77d3b799f37eeca4d
mohsen <mohisls2012@gmail.com>
2024-08-27 19:46:28 +0330

    sd

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,5 @@
+const AppError = require('../utils/appError');
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -8,10 +10,24 @@ const sendErrorDev = (err, res) => {
 };
 
 const sendErrorPro = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (err.isOperatinal) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.error(err);
+
+    res.status(500).json({
+      status: 'error',
+      message: 'somthing went wron',
+    });
+  }
+};
+
+const handelCastErrorDb = (err) => {
+  const message = `invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
 };
 
 module.exports = (err, req, res, next) => {
@@ -22,6 +38,9 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorPro(err, res);
+    let error = { ...err };
+    if ((error.name = 'CastError')) error = handelCastErrorDb(err);
+
+    sendErrorPro(error, res);
   }
 };
