fa45ad1c32e4823f5af392a2463f63bf961e6707
mohsen <mohisls2012@gmail.com>
2024-08-31 21:29:14 +0330

    sd

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,12 +3,10 @@ const User = require('../models/userModel');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 
-const signToken = function () {
-  jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+const signToken = (id) =>
+  jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
-};
-
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
     name: req.body.name,
@@ -16,9 +14,8 @@ exports.signup = catchAsync(async (req, res, next) => {
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
   });
-  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
+
+  const token = signToken(newUser._id);
 
   res.status(201).json({
     status: 'success',
@@ -39,9 +36,16 @@ exports.login = catchAsync(async (req, res, next) => {
     return next(new AppError('incorrect email or password', 401));
   }
   // 3)if evrything ok, send token to client
-  const token = '';
+  const token = signToken(user._id);
+  console.log(token);
   res.status(200).json({
     status: 'success',
     token,
   });
+});
+
+exports.protect = catchAsync(async (req, res, next) => {
+  // 1)getting token and chek if it is there
+  // 2)verification token
+  next();
 });
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -13,11 +13,13 @@ const {
   getMontlyPlan,
 } = require('../controllers/tourControllers');
 
+const { protect } = require('../controllers/authController');
+
 // router.param('id', checkId);
 router.route('/top-5-cheap').get(aliasTopTour, getAllTours);
 router.route('/tour-stats').get(getTourStats);
 router.route('/montly-plan/:year').get(getMontlyPlan);
-router.route('/').get(getAllTours).post(createTour);
+router.route('/').get(protect, getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
