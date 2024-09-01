e783b28bec0e1932877bcb657b0e3803aa25238d
MohiVs <mohisls2012@gmail.com>
2024-09-01 14:20:06 +0330

    sdf

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,4 @@
+const util = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const AppError = require('../utils/appError');
@@ -37,7 +38,7 @@ exports.login = catchAsync(async (req, res, next) => {
   }
   // 3)if evrything ok, send token to client
   const token = signToken(user._id);
-  console.log(token);
+  // console.log(token);
   res.status(200).json({
     status: 'success',
     token,
@@ -46,6 +47,24 @@ exports.login = catchAsync(async (req, res, next) => {
 
 exports.protect = catchAsync(async (req, res, next) => {
   // 1)getting token and chek if it is there
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+  if (!token)
+    next(
+      new AppError('you are not logged in! please log in to get access', 401),
+    );
   // 2)verification token
+  const decoded = await util.promisify(jwt.verify)(
+    token,
+    process.env.JWT_SECRET,
+  );
+  console.log(decoded);
+  // 3) check if user still exists
+  // 4)check if user changed password after token was issued
   next();
 });
