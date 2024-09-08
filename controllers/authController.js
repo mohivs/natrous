1acf795b5142ab2641a47a0fe8c296c66071a5b6
mohsen <mohisls2012@gmail.com>
2024-09-08 20:28:33 +0330

    sdf

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,7 +14,23 @@ const signToken = (id) =>
 // user is where id stores,
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
-  // console.log(token);
+
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
+    ),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === 'production') {
+    cookieOptions.secure = true;
+  }
+
+  res.cookie('jwt', token, cookieOptions);
+
+  // remove password from output
+  user.password = undefined;
+
   res.status(statusCode).json({
     status: 'success',
     token,
