91efed1c0dd3a6b3bec686d0ed86ed15a0376372
MohiVs <mohisls2012@gmail.com>
2024-09-09 14:11:28 +0330

    ssd

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const tourRouter = require('./routes/tourRoutes');
 const globalErrorHandler = require('./controllers/errorController');
@@ -7,14 +11,37 @@ const userRouter = require('./routes/userRoutes');
 const AppError = require('./utils/appError');
 
 const app = express();
-//1) middlewares
+
+//1) global middlewares
+
+//Set security HTTP headers
+app.use(helmet());
+
+// Development loggin
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json());
+//Limit Requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP,please try again about an hour!',
+});
+app.use('/api', limiter);
+
+//Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// data sanitization against NoSql query injection
+app.use(mongoSanitize());
+// data sanitization against XSS
+app.use(xss());
+
+// serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// test middleware
 app.use((req, res, next) => {
   // console.log(req.body);
   next();
